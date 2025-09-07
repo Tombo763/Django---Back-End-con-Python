48de60a078119cbcdf72ab82d46bf92eb786cd52
Tombo763 <olivaresalessandro10@gmail.com>
2025-09-07 18:35:19 -0300

    Agregando "inicio","empresa","contacto", "formulario" y "mvc"

--- holding/empresa/views.py
+++ holding/empresa/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
 
 # Create your views here.
+def inicio(request):
+    return render(request, 'inicio.html', {})
+
+def empresa(request):
+    return render(request, 'empresa.html', {})
+
+def contacto(request):
+    return render(request, 'contacto.html', {})
+
+def formulario(request):
+    return render(request, 'formulario.html', {})
+
+def mvc(request):
+    return render(request, 'mvc.html', {})
