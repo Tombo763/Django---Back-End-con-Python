3c6e35a5a84fb7d112d6669635fdf25f673c3a17
Tombo763 <olivaresalessandro10@gmail.com>
2025-09-07 18:45:33 -0300

    Se pasaron las variables del front end al back end para guardarla en un bloc de notas

--- holding/empresa/views.py
+++ holding/empresa/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
 
 # Create your views here.
 def inicio(request):
@@ -11,6 +11,24 @@ def contacto(request):
     return render(request, 'contacto.html', {})
 
 def formulario(request):
+    if request.method == 'POST':
+        nombre = request.POST.get("nombreAlumno")
+        apellido_p = request.POST.get("apellidoPaterno")
+        apellido_m = request.POST.get("apellidoMaterno")
+        rut = request.POST.get("rutAlumno")
+        fecha_nac = request.POST.get("fechaDeNacimiento")
+
+
+        # Ejemplo: guardar en un archivo
+        archivo_nombre = f"{nombre}_{apellido_p}.txt"
+        with open(archivo_nombre, "w") as f:
+            f.write(f"Nombre: {nombre}\n")
+            f.write(f"Apellido Paterno: {apellido_p}\n")
+            f.write(f"Apellido Materno: {apellido_m}\n")
+            f.write(f"RUT: {rut}\n")
+            f.write(f"Fecha de nacimiento: {fecha_nac}\n")
+        return HttpResponse(f"Formulario recibido de: {nombre} {apellido_p} {apellido_m}")
+
     return render(request, 'formulario.html', {})
 
 def mvc(request):
